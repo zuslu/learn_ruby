e932e3efa964e6d92e354d9d11e04e7efa9819f1
zeynepUslu <zuslu1@binghamton.edu>
2017-01-24 18:59:53 -0500

    Added add, subtract, sum, multiply, power and factorial methods to calculator.rb. Updated the examples for last three functions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
 #write your code here
+def add(first, second)
+	first + second
+end
+
+def subtract(first, second)
+	first - second
+end
+
+def sum(a)
+	sum = 0
+	a.inject(0){|sum,x| sum + x }
+end
+
+def multiply(*numbers)
+  result = 1
+  numbers.each { |n| result = result * n }
+  result
+end
+
+def power(first, second)
+	first ** second
+end
+
+def factorial(number)
+	result = 1
+	for num in 1..number
+		result *= num
+	end
+	result
+end
