3a62eb5ab3920851fe1c562444c51c5a5d3c6c4e
zeynepUslu <zuslu1@binghamton.edu>
2017-01-26 13:30:47 -0500

    Added functions echo, shout, repeat, start_of_word, first_word, titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
 #write your code here
+def echo(arg)
+	arg
+end
+
+def shout(arg)
+	arg.upcase
+end
+
+def repeat(x, y=2)
+	y.times.map{x + ' '}.join.rstrip	
+end
+
+def start_of_word(word, ind)
+	word[0..ind-1]
+end
+
+def first_word(word)
+	word.split(' ')[0]
+end
+
+/def titleize(words)
+	words.split.map(&:capitalize).join(' ')
+end/
+
+def titleize(sentence)
+	stop_words = %w{a an and the or for of nor over}
+  	sentence.split.each_with_index.map{|word, index| stop_words.include?(word) && index > 0 ? word : word.capitalize }.join(" ")
+end
