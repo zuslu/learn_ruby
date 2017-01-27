bfedf5ec1bfb65e12bc26a3a885b4f6a8c9c9c8c
zeynepUslu <zuslu1@binghamton.edu>
2017-01-26 20:56:59 -0500

    Added translate function

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,38 @@
 #write your code here
+def splitter(word)
+	wordArray = word.split(//)
+	i=0
+	while (i < wordArray.length)
+		if (wordArray[i] == "a" || wordArray[i] == "e" || wordArray[i] == "i" || wordArray[i] == "o" || wordArray[i] == "u")
+			wordArray << "ay"
+			break
+		else
+			if(wordArray[i] == "q" && wordArray[i+1] == "u")
+				wordArray << wordArray[i]
+				wordArray << wordArray[i+1]	
+				wordArray.delete_at(0)
+				wordArray.delete_at(0)
+			else
+				wordArray << wordArray[i]
+				wordArray.delete_at(0)
+			end 
+			i = i-1 			
+		end
+		i = i+1
+	end
+	return wordArray.join
+end
+
+def translate(sentence)
+	if sentence.strip.include?(" ")
+		sArr = sentence.split(/ /)
+		j= 0
+		while(j < sArr.length)
+			sArr[j]=splitter(sArr[j])
+			j = j +1
+		end
+		return sArr.join(' ').squeeze(' ') 
+	else
+		splitter(sentence)
+	end
+end
